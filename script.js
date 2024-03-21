a454c3c5e7e5218965b5d59d195668e54c9d3679
Sainath <71361447+sainath161@users.noreply.github.com>
2024-03-21 20:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function setBackgroundImage() {
+    var imageUrl;
+    if (window.innerHeight > window.innerWidth) {
+        // Portrait Mode
+        imageUrl = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg';
+    } else {
+        // Landscape Mode
+        imageUrl = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg';
+    }
+    document.body.style.backgroundImage = 'url(' + imageUrl + ')';
+}
+
+// Call the function when the page loads and when the window is resized
+window.onload = setBackgroundImage;
+window.onresize = setBackgroundImage;
